b514b77655b4435036dfadde334e843a2ad7ea8d
Alex Santos <alex-ps@live.com>
2026-08-13 20:48:35 -0300

    chore: cria estrutura inicial do projeto

---
+++ src/main/java/br/com/confirmacao/ConfirmacaoConsultasApplication.java
@@ -0,0 +1,13 @@
+package br.com.confirmacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmacaoConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmacaoConsultasApplication.class, args);
+	}
+
+}
